adb1d7bd0338ae1575dcfdfa4ac84eee8b0afdc7
MingPV <jeung.pavee@gmail.com>
2025-05-20 15:13:40 +0700

    Add Transition

--- pages/index.vue
+++ pages/index.vue
@@ -15,57 +15,44 @@ const store = useSceneStore();
     <div
       class="absolute top-0 left-0 w-full h-[100vh] text-center z-40 text-white"
     >
-      <!-- <h1 class="text-4xl font-bold">
-        {{ sectionContents[store.currentSection].title }}
-      </h1>
-      <p class="text-xl mt-2">
-        {{ sectionContents[store.currentSection].desc }}
-      </p> -->
-      <!-- <template v-if="store.currentSection === 0">
+      <Transition name="fade" mode="out-in">
         <KeepAlive>
-          <HelloContent />
+          <HelloContent v-show="store.currentSection === 0" />
         </KeepAlive>
-      </template>
-      <template v-else-if="store.currentSection === 1">
-        <KeepAlive>
-          <AboutContent />
-        </KeepAlive>
-      </template>
-      <template v-else-if="store.currentSection === 2">
-        <KeepAlive>
-          <ExperienceContent />
-        </KeepAlive>
-      </template>
-      <template v-else-if="store.currentSection === 3">
-        <KeepAlive>
-          <ProjectContent />
-        </KeepAlive>
-      </template>
-      <template v-else-if="store.currentSection === 4">
-        <KeepAlive>
-          <ContactContent />
-        </KeepAlive>
-      </template> -->
-
-      <KeepAlive
-        ><HelloContent v-show="store.currentSection === 0"
-      /></KeepAlive>
-      <KeepAlive
-        ><AboutContent v-show="store.currentSection === 1"
-      /></KeepAlive>
-      <KeepAlive
-        ><ExperienceContent v-show="store.currentSection === 2"
-      /></KeepAlive>
-      <KeepAlive
-        ><ProjectContent v-show="store.currentSection === 3"
-      /></KeepAlive>
-      <KeepAlive
-        ><ContactContent v-show="store.currentSection === 4"
-      /></KeepAlive>
+      </Transition>
+      <Transition name="fade" mode="out-in"
+        ><KeepAlive
+          ><AboutContent v-show="store.currentSection === 1" /></KeepAlive
+      ></Transition>
+      <Transition name="fade" mode="out-in"
+        ><KeepAlive
+          ><ExperienceContent v-show="store.currentSection === 2" /></KeepAlive
+      ></Transition>
+      <Transition name="fade" mode="out-in"
+        ><KeepAlive
+          ><ProjectContent v-show="store.currentSection === 3" /></KeepAlive
+      ></Transition>
+      <Transition name="fade" mode="out-in"
+        ><KeepAlive
+          ><ContactContent v-show="store.currentSection === 4" /></KeepAlive
+      ></Transition>
     </div>
 
     <ThreeScene />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
