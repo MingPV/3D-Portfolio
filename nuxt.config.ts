e1d6c9860d154a0bf504356fe984388e4ad5e7f9
MingPV <jeung.pavee@gmail.com>
2025-05-20 18:29:05 +0700

    Update favicon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "MingPV Portfolio",
+      meta: [{ name: "description", content: "mingpv portfolio website" }],
+    },
+  },
+
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   modules: [
